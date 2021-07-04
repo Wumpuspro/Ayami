0e5bd81f2aa554e7fff81f16487c1ced6d845277
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-07-04 18:09:51 +0545

    fix database error

--- events/ready.js
+++ events/ready.js
@@ -1,43 +1,98 @@
+const CronJob = require("cron").CronJob;
+const Discord = require("discord.js");
+const Constants = require("../helpers/constants");
+
 module.exports = class {
     constructor (client) {
         this.client = client;
     }
 
-    
     async run () {
 
-        const client = this.client;
-       
-        if(!process.argv.includes("--uncache")) await this.client.wait(1000);
-        let invites = {};
-        let startAt = Date.now();
+        this.client.user.setActivity("+help | manage-invite.xyz");
+        setInterval(() => {
+            this.client.user.setActivity("+help | manage-invite.xyz");
+        }, 60000*60);
+        this.client.log("Shard #"+this.client.shard.ids[0]+" has started.", "log");
+        this.client.functions.postTopStats(this.client);
+
+        if (!process.argv.includes("--uncache")) await this.client.wait(1000);
+        const invites = {};
+        const startAt = Date.now();
         this.client.fetching = true;
 
+        const premiumGuildsID = await this.client.database.fetchPremiumGuildIDs();
+        this.client.log(`Shard #${this.client.shard.ids[0]} is ready!`);
         await this.client.functions.asyncForEach(this.client.guilds.cache.array(), async (guild) => {
-            let i = process.argv.includes("--uncache") ? new Map() : (guild.me.hasPermission("MANAGE_GUILD") ? await guild.fetchInvites().catch(() => {}) : new Map());
-            invites[guild.id] = i || new Map();
+            if (premiumGuildsID.includes(guild.id)){
+                const member = await guild.members.fetch(this.client.user.id).catch(() => {});
+                const i = process.argv.includes("--uncache") ? null : (member.permissions.has(Discord.Permissions.FLAGS.MANAGE_GUILD) ? await guild.fetchInvites().catch(() => {}) : null);
+                invites[guild.id] = i || null;
+            }
         });
         this.client.invitations = invites;
         this.client.fetched = true;
         this.client.fetching = false;
-        if(this.client.shard.ids.includes(0)) console.log("=================================================");
-        this.client.user.setPresence({ status: "online", activity:{name: `@Ayami - 1.0.0 | Shard: ${this.client.shard.ids[0]} | type +help`, type: "PLAYING" }});
-        setInterval(() => {
-            this.client.user.setPresence({ status: "online", activity:{name: `@Ayami - 1.0.0 | Shard: ${this.client.shard.ids[0]} | type +help `, type: "PLAYING" }});
-        }, 60000*60);
-        console.log(`\x1b[32m%s\x1b[0m`, `SHARD [${this.client.shard.ids[0]}]`, "\x1b[0m", `Invitations retrieved in  ${Date.now() - startAt} ms.`);
+        if (this.client.shard.ids.includes(0)) console.log("=================================================");
+        console.log("\x1b[32m%s\x1b[0m", `SHARD [${this.client.shard.ids[0]}]`, "\x1b[0m", `Invites fetched in ${Date.now() - startAt} ms.`);
         console.log("=================================================");
-        if(this.client.shard.ids.includes(this.client.shard.count-1)){
-            console.log("Ready. Saved as "+this.client.user.tag+".Some statistics:\n");
-            this.client.shard.broadcastEval(() => {
-                console.log(`\x1b[32m%s\x1b[0m`, `SHARD [${this.shard.ids[0]}]`, "\x1b[0m", `Serving ${this.users.cache.size} members at ${this.guilds.cache.size} servers.`);
+        if (this.client.shard.ids.includes(this.client.shard.count-1)){
+            console.log("Ready. Logged as "+this.client.user.tag+". Some stats:\n");
+            this.client.shard.broadcastEval((client) => {
+                console.log("\x1b[32m%s\x1b[0m", `SHARD [${client.shard.ids[0]}]`, "\x1b[0m", `Serving ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`);
             });
         }
-
-        if(this.client.shard.ids.includes(0) && !this.client.spawned){
-            this.client.dash.load(this.client);
+        this.client.ipc.load(this.client);
+        if (this.client.shard.ids.includes(0) && !this.client.spawned){
+            new CronJob("0 5 0 * * *", async () => {
+                // tous les abonnements qui ont expiré il y a trois jours au moins
+                this.client.database.fetchNewlyCancelledPayments().then(async (paymentsData) => {
+                    console.log(`Envoi de ${paymentsData.length} notifications`);
+                    paymentsData.forEach(async (paymentData) => {
+                        const user = await this.client.users.fetch(paymentData.payerDiscordID);
+                        const guildNames = await this.client.shard.broadcastEval((client, guildID) => {
+                            const guild = this.guilds.cache.get(guildID);
+                            if (guild) return guild.name;
+                        }, { context: paymentData.guildID });
+                        const guildNameFound = guildNames.find((r) => r);
+                        if (!guildNameFound) {
+                            return this.client.database.setPaymentRemindSent({
+                                paymentID: paymentData.paymentID, 
+                                subID: paymentData.subID,
+                                success: false,
+                                kicked: true
+                            });
+                        }
+                        const beg = paymentData.subLabel === "Trial Version" ? "Your trial period" : "Your premium subscription";
+                        const embed = new Discord.MessageEmbed()
+                            .setAuthor(`Hello, ${user.username}`)
+                            .setDescription(`${beg} for **${guildNameFound}** expires in 72 hours! Click [here](https://localhost:3000/manage/${paymentData.guildID}/createsub) to continue to use the bot, the price is $2 per month.`)
+                            .setColor(Constants.Embed.COLOR)
+                            .setFooter(Constants.Embed.FOOTER);
+                        const send = () => new Promise((resolve) => user.send({ embeds: [embed] }).then(resolve(true)).catch(resolve(false)));
+                        this.client.database.setPaymentRemindSent({
+                            paymentID: paymentData.paymentID, 
+                            subID: paymentData.subID,
+                            success: await send(),
+                            kicked: false
+                        });
+                    });
+                });
+            }, null, true, "America/Los_Angeles");
         }
-        
+
+        new CronJob("0 */15 * * * *", async () => {
+            if (this.client.fetched){
+                const guildsToFetch = this.client.guilds.cache.filter((guild) => !this.client.invitations[guild.id]).array();
+                this.client.log(`${guildsToFetch.length} guilds need to be fetched`);
+                await this.client.functions.asyncForEach(guildsToFetch, async (guild) => {
+                    const member = await guild.members.fetch(this.client.user.id).catch(() => {});
+                    const i = process.argv.includes("--uncache") ? null : (member.permissions.has(Discord.Permissions.FLAGS.MANAGE_GUILD) ? await guild.fetchInvites().catch(() => {}) : null);
+                    this.client.invitations[guild.id] = i || null;
+                });
+                this.client.fetched = true;
+            }
+        }, null, true, "America/Los_Angeles");
 
     }
 };

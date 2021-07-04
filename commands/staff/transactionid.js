1cad53b87ca395fb18da269a3a3a8a7e147db89f
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-07-04 21:53:34 +0545

    1.1.0

---
+++ commands/staff/transactionid.js
@@ -0,0 +1,25 @@
+const Command = require("../../structures/Command.js");
+const Constants = require("../../helpers/constants");
+
+module.exports = class extends Command {
+    constructor (client) {
+        super(client, {
+            name: "transactionid",
+            enabled: true,
+            aliases: [ "transac" ],
+            clientPermissions: [],
+            permLevel: 5
+        });
+    }
+
+    async run (message, args) {
+
+        const transactionID = args[0];
+        if (!transactionID) return message.error("You must specify a valid transaction ID!");
+
+        const transactionData = await this.client.mongodb.fetchTransactionData(transactionID);
+        if (!transactionData) return message.error("No payment found for that transaction ID...");
+
+        return message.channel.send(`${Constants.Emojis.SUCCESS} | Here is the subscription related to this transaction ID: \`${transactionData.subID}\`. (\`${transactionData.guildID}\`)`);
+    }
+};

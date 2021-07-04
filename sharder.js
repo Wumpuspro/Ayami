de4be24509e14fac4128e84f803e5ca72dd66c80
Wumpuspro <86592522+Wumpuspro@users.noreply.github.com>
2021-07-04 09:06:11 +0545

    Create sharder.js

---
+++ sharder.js
@@ -0,0 +1,12 @@
+  
+const { ShardingManager } = require("discord.js");
+const manager = new ShardingManager("./index.js", {
+    token: require("./config").token,
+    totalShards: require("./config").shardCount,
+    shardArgs: [ ...process.argv, ...[ "--sharded" ] ]
+});
+
+const log = require("./helpers/logger");
+log("Ayami has been started! 🚀\n");
+
+manager.spawn();

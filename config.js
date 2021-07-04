9083f598e012f90c647ea6ee269c0b53350c2ef3
Wumpuspro <86592522+Wumpuspro@users.noreply.github.com>
2021-07-04 10:44:15 +0545

    fill it and secure your dependencies

--- config.js
+++ config.js
@@ -1,22 +1,23 @@
 module.exports = {
     ipcServerPort: 3200,
-    token:          "XXXXXXXXXXX",
-    shardCount:     2,
- 
-        id: "XXXXXXXXXXX",
-        token: "XXXXXXXXXXX"
+    token:          "ODUyNDE5MTk1NDMzNTE3MDU2.YMGjPQ.jpiwdWUpvcNQs_lm6ubNdpKsyP8",
+    shardCount:     0,
+  statusWebhook: {
+        id: "860882269119578132",
+        token: "vMhHSi-Y04o-BNHEHH_JmPo95AOQ05hQgs8K457crOpfqCTBw_jxnXcZzko3_Q2GR0Mj"
     },
     
-    addLogs:        "XXXXXXXXXXX",
-    removeLogs:     "XXXXXXXXXXX",
-    premiumLogs:    "XXXXXXXXXXX",
-    voteLogs:       "XXXXXXXXXXX",
+    addLogs:        "859058872273993801",
+    removeLogs:     "859058872273993801",
+    premiumLogs:    "859058872273993801",
+    voteLogs:       "859058872273993801",
     
-    discord:        "XXXXXXXXXXX",
+    discord:        "https://discord.gg/ntxtVBA6re",
     prefix:         "+",
-    owners: [ "XXXXXXXXXXX", "XXXXXXXXXXX" ],
-    modRole:        "XXXXXXXXXXX",
-    premiumRole:    "XXXXXXXXXXX",
-    supportServer:  "XXXXXXXXXXX",
+    owners: [ "852219497763045398", "697465326383792158" ],
+    modRole:        "861108743840333825",
+    premiumRole:    "861108152010014790",
+    supportServer:  "https://discord.gg/ntxtVBA6re",
     sentryDSN:      "XXXXXXXXXXX",
+    mongodb: "",
 };

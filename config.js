b0a65682cd249e4b50dfe6838d6e9d32c6271a18
Luminios <86885455+Luminios@users.noreply.github.com>
2021-07-04 10:35:22 +0545

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    ipcServerPort: 3200,
+    token:          "XXXXXXXXXXX",
+    shardCount:     2,
+ 
+        id: "XXXXXXXXXXX",
+        token: "XXXXXXXXXXX"
+    },
+    
+    addLogs:        "XXXXXXXXXXX",
+    removeLogs:     "XXXXXXXXXXX",
+    premiumLogs:    "XXXXXXXXXXX",
+    voteLogs:       "XXXXXXXXXXX",
+    
+    discord:        "XXXXXXXXXXX",
+    prefix:         "+",
+    owners: [ "XXXXXXXXXXX", "XXXXXXXXXXX" ],
+    modRole:        "XXXXXXXXXXX",
+    premiumRole:    "XXXXXXXXXXX",
+    supportServer:  "XXXXXXXXXXX",
+    sentryDSN:      "XXXXXXXXXXX",
+};

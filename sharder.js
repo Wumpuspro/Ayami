58edbf8ee21469a334e02e54e082fac7fc77819e
Wumpuspro <86592522+Wumpuspro@users.noreply.github.com>
2021-07-04 09:09:38 +0545

    shard to make bot strong

--- sharder.js
+++ sharder.js
@@ -1,6 +1,6 @@
   
 const { ShardingManager } = require("discord.js");
-const manager = new ShardingManager("./index.js", {
+const manager = new ShardingManager("./ayami.js", {
     token: require("./config").token,
     totalShards: require("./config").shardCount,
     shardArgs: [ ...process.argv, ...[ "--sharded" ] ]

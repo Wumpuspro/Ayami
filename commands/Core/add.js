a8dd1d7eafa9f6c6797529221a5a79e4cfc50329
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-07-04 14:13:19 +0545

    fix with #embed

---
+++ commands/Core/add.js
@@ -0,0 +1,30 @@
+const Command = require("../../structures/Command.js"),
+    Discord = require("discord.js");
+
+module.exports = class extends Command {
+    constructor (client) {
+        super(client, {
+            name: "add",
+            enabled: true,
+            aliases: [],
+            clientPermissions: [ "EMBED_LINKS" ],
+            permLevel: 0
+        });
+    }
+
+    async run (message) {
+
+        const embed = new Discord.MessageEmbed()
+            .setAuthor("Ayami", this.client.user.displayAvatarURL())
+            .setDescription(message.translate("core/add:CONTENT", {
+                clientID: this.client.user.id
+            }))
+            .setColor(this.client.config.color)
+            .setFooter(message.translate("core/add:REQUEST_BY", {
+                username: message.author.username
+            }), message.author.displayAvatarURL());
+        message.channel.send({ embeds: [embed] });
+
+    }
+
+};

8e3c868494af2fc192b3e28114f3b66758521d40
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-07-04 13:22:30 +0545

    fix #embed

---
+++ helpers/constants.js
@@ -0,0 +1,27 @@
+const Links = {
+    DISCORD:    "https://discord.gg/ntxtVBA6re",
+    DASHBOARD:  "local:3000"
+};
+
+const Embed = {
+    COLOR:      "#F4831B",
+    FOOTER:     "Ayami bot #comeback 🚀"
+};
+
+const Emojis = {
+    SUCCESS:    "<:success:638755721848160307>",
+    ERROR:      "<:error:638756167446822914>",
+    ONLINE:     "<:online:639063781481119745>",
+    DND:        "<:dnd:639420780140298260>",
+    OFFLINE:    "<:offline:639420654898118667>",
+    IDLE:       "<:idle:639420564142030848>",
+    LOADING:    "<a:loading:641653408255967268>",
+    WARN:       ":warning:",
+    UPGRADE:    "<:upgrade:711127764761313342>"
+};
+
+module.exports = {
+    Links,
+    Embed,
+    Emojis
+};
